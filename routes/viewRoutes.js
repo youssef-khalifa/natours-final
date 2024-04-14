f04f4a58e5b63203a4117ef594f67dc98d1c6896
youssef-khalifa <68951546+youssef-khalifa@users.noreply.github.com>
2024-04-14 09:42:19 +0200

    Update viewRoutes.js

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -12,7 +12,7 @@ router.get('/me', authController.protect, viewsController.getAccount);
 
 router.get(
   '/my-tours',
-  // bookingController.createBookingCheckout,
+  bookingController.createBookingCheckout,
   authController.protect,
   viewsController.getMyTours
 );
