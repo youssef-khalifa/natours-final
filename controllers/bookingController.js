7713de6763e3690e20ae1dc45755ecf520039422
youssef-khalifa <68951546+youssef-khalifa@users.noreply.github.com>
2024-04-14 09:26:38 +0200

    Update bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -29,7 +29,9 @@ exports.getCheckoutSession = catchAsynch(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [
+            `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
+        ],
           },
         },
         quantity: 1,
