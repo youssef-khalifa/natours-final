3960d63469c71e3a0babc42ceb96e75ee7d83f9a
youssef-khalifa <68951546+youssef-khalifa@users.noreply.github.com>
2024-04-14 09:34:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const tourRourter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 
 const viewRouter = require('./routes/viewRoutes');
 
@@ -42,6 +43,11 @@ const limiter = rateLimit({
   message: 'too many requests from this ip,please try agai in 1 hour',
 });
 app.use('/api', limiter);
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 // Body parser, reading data from body into req.body
 
 app.use(express.json({ limit: '10kb' }));
