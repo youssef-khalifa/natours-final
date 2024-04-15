d8af9c90378daf043f663ede93674328757358b8
youssef-khalifa <68951546+youssef-khalifa@users.noreply.github.com>
2024-04-15 09:56:24 +0200

    Update bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -13,10 +13,10 @@ exports.getCheckoutSession = catchAsynch(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
-    //   req.params.tourId
-    // }&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
+    // success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
